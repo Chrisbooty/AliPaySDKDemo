9968193581889217c0fbe7c184cc8405074571f8
Chris <ChrisW@chrisdemac.local>
2016-08-13 21:18:54 +0800

    Initial Commit

---
+++ ChrisAliPayDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ChrisAliPayDemo
+//
+//  Created by Chris on 16/8/13.
+//  Copyright © 2016年 Chris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ChrisAliPayDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ChrisAliPayDemo
+//
+//  Created by Chris on 16/8/13.
+//  Copyright © 2016年 Chris. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
