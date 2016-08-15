a129abde325ba6557a5377cf00fd1f4fa1040fd5
Chris <ChrisW@chrisdemac.local>
2016-08-15 23:50:32 +0800

    完成AliPaySDK集成

---
+++ ChrisAliPayDemo/AliPaySDK/AlipayConfig.h
@@ -0,0 +1,21 @@
+//
+//  AlipayConfig.h
+//
+//  Created by wuyiguang on 15/3/13.
+//  Copyright (c) 2016年 YG. All rights reserved.
+//
+
+#ifndef AlipayConfig_h
+#define AlipayConfig_h
+
+// 每一个被签约的商户都有一个唯一的 parnter 和 selller
+// 合作身份者id，以2088开头的16位纯数字
+#define PartnerID @"2088801142678498"
+
+// 收款支付宝账号
+#define SellerID  @"lisa@newv.com.cn"
+
+// 商户私钥，自助生成
+#define PartnerPrivKey @"MIICdgIBADANBgkqhkiG9w0BAQEFAASCAmAwggJcAgEAAoGBAKADO4HcskWfaWCSLSdCqPMWymC2m3paALX6jVjpz/eIFajO/hbOec1IZB8yLImClKTyke9kGy2IwVSt1QNugxx/u6lUEoE6LeO2LKR37YnopdgKxXVITX5Ymn0yJdI3WWFiqfcFXzAHq3D5Il9zfTVRyOTHfRTC05EML7F1hV/1AgMBAAECgYBeUKlxqQk3OngdYOvWeVcmOae+C8RnAMfse6t23hIkAAVsQ93GyZtHocTKEoPn5Z0CAKx+I05Vr4btB61H4YrLga89mbXjP+YoYejNRVoZBjEpkf7NgMh7ScQIhx3hHk2IuCO6syz/cR5zNWpK47gMSWtS3/xoWjX9aicKcsTNgQJBANQd3qZZeo1XTi4qugp8AAvCwsy4IGM1LztahKIll+XMTLHxF6e5WTEQTuRe6fQzfbP7emNX3aAwqLRhgez+hmECQQDBHdOGsuqGQkes1VO9C9zEOegjEgv+Dzxq3Wj8AeVzO4idUJ2Gl6IV4EWflev9kZGX6CXFjJ3RngM6dsoejpoVAkEAj36Rc7mOhXVtZx/ycUtHgK1FuNZK2rJM/HsUxNhntMaLj8kIdqeVpfJhXG61GEWJISvbtL7pKAgi6LwZ9+iLoQJAWO5HTqxt284B+9FxcolX7PVNtXjGFQUnKX80rXiiFWLBEtDg+e4yMijJZyg/ONIkXfQGEOckdjdx/SZfBZtd0QJAPL0+YBO5Bvbb4/VRIeoww+4pAd46f9Ews0a0gYjOrTL+tNomvVet92HgJJQIEIoLbqh4NTDJDFgC8rmh7UBeSg=="
+
+#endif
--- ChrisAliPayDemo/ViewController.m
+++ ChrisAliPayDemo/ViewController.m
@@ -7,6 +7,12 @@
 //
 
 #import "ViewController.h"
+#import <AliPaySDK/AlipaySDK.h>
+#import "Order.h"
+#import "AlipayConfig.h"
+#import "AlipayConfig.h"
+#import "DataSigner.h"
+
 
 @interface ViewController ()
 
@@ -17,6 +23,43 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+}
+- (IBAction)click {
+    Order *order = [[Order alloc] init];
+    order.partner = PartnerID;
+    order.sellerID = SellerID;
+    order.outTradeNO = @"123456789"; //订单ID（由商家?自?行制定）
+    order.subject = @"布加迪威龙"; //商品标题
+    order.body = @"牛逼红花那个的车"; //商品描述
+    order.totalFee = [NSString stringWithFormat:@"%.2f",0.01f]; //商品价格
+    order.notifyURL = @"http://www.test.com"; //回调URL
+    order.service = @"mobile.securitypay.pay";
+    order.paymentType = @"1";
+    order.inputCharset = @"utf-8";
+    order.itBPay = @"30m";
+    
+    //应用注册scheme,在AlixPayDemo-Info.plist定义URL types
+    NSString *appScheme = @"alisdkdemo";
+    
+    //将商品信息拼接成字符串
+    NSString *orderSpec = [order description];
+    NSLog(@"orderSpec = %@",orderSpec);
+    
+    //获取私钥并将商户信息签名,外部商户可以根据情况存放私钥和签名,只需要遵循RSA签名规范,并将签名字符串base64编码和UrlEncode
+    id<DataSigner> signer = CreateRSADataSigner(PartnerPrivKey);
+    NSString *signedString = [signer signString:orderSpec];
+    
+    //将签名成功字符串格式化为订单字符串,请严格按照该格式
+    NSString *orderString = nil;
+    if (signedString != nil) {
+        orderString = [NSString stringWithFormat:@"%@&sign=\"%@\"&sign_type=\"%@\"",
+                       orderSpec, signedString, @"RSA"];
+        
+        [[AlipaySDK defaultService] payOrder:orderString fromScheme:appScheme callback:^(NSDictionary *resultDic) {
+            //【callback处理支付结果】
+            NSLog(@"reslut = %@",resultDic);
+        }];
+    }
 }
 
 - (void)didReceiveMemoryWarning {
